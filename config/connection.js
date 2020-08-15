8f8147ffb023de0ad734a3452fb98d5086d9825b
Don Tran <mr.dontran@gmail.com>
2020-08-15 19:57:59 -0400

    edit connection for jawdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 var mysql = require("mysql");
 
 require("dotenv").config()
+let connection;
 
 
-let connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: process.env.USER,
   password: process.env.PASS,
   database: process.env.DB_NAME
 });
-
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
